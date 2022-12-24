c97a6a35ce210590012eaed790ad8714ec47f231
OfferedDegrees <102624427+OfferedDegrees@users.noreply.github.com>
2022-12-24 00:41:22 -0800

    Add files via upload
    Nail Salon FIles

---
+++ NailSalon-20221224T082108Z-001/NailSalon/scripts.js
@@ -0,0 +1,27 @@
+const toggleButton = document.getElementsByClassName("toggle-button")[0]
+const navbarLinks = document.getElementsByClassName("nav-right")[0]
+
+toggleButton.addEventListener('click', () => { 
+  navbarLinks.classList.toggle("active")
+})
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 6000); // Change image every 2 seconds
+}
+
+var frmvalidator  = new Validator("contactform");
+frmvalidator.addValidation("name","req","Please provide your name");
+frmvalidator.addValidation("email","req","Please provide your email");
+frmvalidator.addValidation("email","email",
+  "Please enter a valid email address");
